6fac9c72f692a5f93f73f921e109f9253274f787
Diego <dtellez95@gmail.com>
2022-01-05 12:56:26 +0000

    feat: basic structure and dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from './App';
+import 'antd/dist/antd.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router><App /></Router>, document.getElementById('root'));
